652f9094f49233c607106a77ffcc65d5a116e068
prxbdead <roland.gerg.123@gmail.com>
2026-07-07 14:58:04 +0300

    turn portfolio into gh pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,10 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: 'https://prxb.hu/',
+  base: '/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
